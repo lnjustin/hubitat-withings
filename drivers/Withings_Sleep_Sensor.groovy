4f7a7efa43d3bb00d8d731f58f80038307cd0b0a
bfara83 <bfara83@gmail.com>
2023-03-10 09:58:18 -0500

    Update Withings_Sleep_Sensor.groovy
    Add Sleep Sensor and Switch that sets Sleeping on and off.  I use the switch as an easier way to setup a RM rule to trigger my night routines to set the Ring Security when me and my wife is in bed.   The Sleep Sensor I use in my dashboard.

--- drivers/Withings_Sleep_Sensor.groovy
+++ drivers/Withings_Sleep_Sensor.groovy
@@ -12,6 +12,8 @@
 metadata {
     definition(name: "Withings Sleep Sensor", namespace: "lnjustin", author: "dmeglio@gmail.com, lnjustin") {
         capability "Sensor"
+        capability "SleepSensor"
+        capability "Switch"
         capability "PresenceSensor"
         
         attribute "wakeupDuration", "number"
@@ -55,4 +57,22 @@ metadata {
         attribute "awakeAfterSleepQuality", "string"
         attribute "outOfBedCount", "number"
     }
+}
+
+def asleep() {
+	on()
+}
+
+def awake() {
+	off()
+}
+
+def on() {
+    sendEvent(name: "sleeping", value: "sleeping")
+    sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+    sendEvent(name: "sleeping", value: "not sleeping")
+    sendEvent(name: "switch", value: "off")
 }
